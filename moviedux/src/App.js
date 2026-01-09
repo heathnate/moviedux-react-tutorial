ce799ae5485e1bd2e0f2b0c61a6e5955f33a7907
nheath <nate.heath@nlign.com>
2026-01-09 14:07:23 -0500

    fixing dependencies

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -3,14 +3,33 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<WatchList />}></Route>
+          </Routes>
+        </Router>
       </div>
+
       <Footer></Footer>
     </div>
   );
